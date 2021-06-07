d50d04811420d91fd5828ec371e650fef2a9dd7c
blbadger <blbadger10@gmail.com>
2021-06-07 11:35:18 -0400

    fixed ackermann

---
+++ ackermann.cpp
@@ -0,0 +1,21 @@
+// ackermanb.cpp
+// computes the value of the Ackermann
+// function
+
+#include <iostream>
+
+int ackermann(int m, int n) {
+	if (m == 0) return n+1;
+
+	else if (n == 0) return ackermann(m-1, 1);
+	
+	else return ackermann(m-1, ackermann(m, n-1));
+	
+}
+
+int main() {
+	int m = 4;
+	int n = 1;
+	std::cout << ackermann(m, n);
+	return 0;
+}
